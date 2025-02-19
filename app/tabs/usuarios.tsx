8d44a73a3292279a2365fde1068d3ca851f50b42
rafael arcanjo <rafaelarcanjo129@hotmail.com>
2025-02-19 10:54:10 -0300

    configuracao do eslint

--- app/tabs/usuarios.tsx
+++ app/tabs/usuarios.tsx
@@ -1,50 +1,50 @@
-import React, {useState} from 'react';
-import {Text, View, Button, ScrollView} from 'react-native';
-import axios from 'axios';
-
-
+import React, { useState } from "react";
+import { Text, View, Button, ScrollView } from "react-native";
+import axios from "axios";
 
 const UsuariosPage = () => {
-  const [datas, setData] = useState([])
-
+  const [datas, setData] = useState([]);
 
   const TouchablesOut = async () => {
-    await axios.get("http://10.2.3.59:3000/usuarios",
-    ).then(async function (response) {
-      setData(response.data.users)
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-
-  }   
-
- 
+    await axios
+      .get("http://10.2.3.59:3000/usuarios")
+      .then(async function (response) {
+        setData(response.data.users);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   return (
     <ScrollView>
-    <View style={{padding: 50,marginTop: 100, gap: 10}}>
-      <Text style={{fontSize: 30, textAlign: 'center'}}>Lista de usuarios</Text>
-        
-        <Button
-              title="buscar"
-              color="#6366f1"
-              onPress={TouchablesOut}
-            />
-     
+      <View style={{ padding: 50, marginTop: 100, gap: 10 }}>
+        <Text style={{ fontSize: 30, textAlign: "center" }}>
+          Lista de usuarios
+        </Text>
 
-     <View>
-        {datas && datas.map((data) => {
-          return (
-            <View key={data.id} style={{ padding:10, borderWidth:1, borderColor: 'black', margin:10}}>
-              <Text >ID: {data.id}</Text>
-              <Text >Nome: {data.name}</Text>
-            </View>
-          );
-        })}
+        <Button title="buscar" color="#6366f1" onPress={TouchablesOut} />
+
+        <View>
+          {datas &&
+            datas.map((data) => {
+              return (
+                <View
+                  key={data.id}
+                  style={{
+                    padding: 10,
+                    borderWidth: 1,
+                    borderColor: "black",
+                    margin: 10,
+                  }}
+                >
+                  <Text>ID: {data.id}</Text>
+                  <Text>Nome: {data.name}</Text>
+                </View>
+              );
+            })}
+        </View>
       </View>
-
-    </View>
     </ScrollView>
   );
 };
